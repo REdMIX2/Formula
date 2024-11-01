e16e3c4256b30a110f05fd6a9a6761a29f8f3374
REdMIX2 <REdMIX2>
2024-11-01 23:54:51 +0300

    Add global var in config and edit formula benhmark

--- benchmark/examples/benchmarkParse.cpp
+++ benchmark/examples/benchmarkParse.cpp
@@ -1,12 +1,12 @@
 #include "benchmark/cppbenchmark.h"
-
-#include <math.h>
+#include <formula.hpp>
+using namespace formula;
 
 // Benchmark sin() call for 5 seconds (by default).
 // Make 5 attemtps (by default) and choose one with the best time result.
-BENCHMARK("sin")
+BENCHMARK("formula-parse")
 {
-    sin(123.456);
+    Formula f = "sin(pi/12)^2 + 0.65*(-8.32 + 9) + 3 / tan(pi/4)";
 }
 
 BENCHMARK_MAIN()
